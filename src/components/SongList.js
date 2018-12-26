c24a3575634d5e53c0d7273b7e265063a9184fe5
NemanjaMandic <nemanja.mandic@folkmatic.com>
2018-12-26 10:10:59 +0100

    added song detail

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
+
 
 class SongList extends Component{
 
+
   gimmeSongs(){
-    return this.props.songs.map( (li, index) => {
+    return this.props.songs.map( (song, index) => {
 
     return(
       <div className="item" key={index}>
         <div className="right floated content">
-          <button className="ui button primary">Select</button>
+          <button
+            className="ui button primary"
+            onClick={() => this.props.selectSong(song) }>Select</button>
         </div>
 
         <div className="content">
-          <p>{ li.title }</p>
+          <p>{ song.title }</p>
         </div>
       </div>
     );
@@ -21,8 +26,7 @@ class SongList extends Component{
     })
   }
   render(){
-    console.log(this.props.songs.map(iks => iks.title));
-
+    //console.log(this.props.songs.map(iks => iks.title));
 
     return(
       <div className="ui divided list">
@@ -34,10 +38,15 @@ class SongList extends Component{
   }
 }
 
+const mapDispatchToProps = (dispatch) => {
+  console.log(dispatch)
+}
 const mapStateToProps = (state) => {
-
+  console.log(state)
   return { songs: state.songs };
 
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {
+  selectSong,
+})(SongList);
